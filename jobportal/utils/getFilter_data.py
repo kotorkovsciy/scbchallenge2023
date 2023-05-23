ab5e9fdd825426ba0a459852a7a9b2c2d2302a54
Angeloffy <angeloffy.work@gmail.com>
2023-05-24 00:53:14 +0300

    feat(jobportal/utils/getFilter_data): Added new class FilterData

---
+++ jobportal/utils/getFilter_data.py
@@ -0,0 +1,13 @@
+import requests
+
+class FilterData():
+
+    def __init__(self):
+        self.area_url = "https://api.hh.ru/areas/"
+        self.specializations_url = "https://api.hh.ru/specializations"
+
+    def get_area(self):
+        return requests.get(self.area_url).json()[0]
+
+    def get_specializations(self):
+        return requests.get(self.specializations_url).json()[0]
