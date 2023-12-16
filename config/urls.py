078ca9792c29bc6bca96e08acba53de215180a72
Kotorokovsciy <kotorkovsciy@gmail.com>
2023-12-16 10:30:56 +0100

    chore: add urls for media and static

--- config/urls.py
+++ config/urls.py
@@ -1,8 +1,13 @@
 from django.contrib import admin
 from django.urls import include
 from django.urls import path
+from django.urls import re_path as url
+from django.conf import settings
+from django.views.static import serve
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("jobportal.urls")),
+    url(r'^media/(?P<path>.*)$', serve,{'document_root': settings.MEDIA_ROOT}),
+    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
 ]
